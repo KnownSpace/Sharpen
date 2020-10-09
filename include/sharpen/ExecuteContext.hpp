56d91830cb0586524d236051c2c272e9374225bc
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-09 17:58:33 +0800

    Update ExecuteContext.hpp

--- include/sharpen/ExecuteContext.hpp
+++ include/sharpen/ExecuteContext.hpp
@@ -2,6 +2,8 @@
 #ifndef _SHARPEN_EXECUTECONTEXT_HPP
 #define _SHARPEN_EXECUTECONTEXT_HPP
 
+#include <functional>
+
 #include "SystemMacro.hpp"
 
 #ifdef SHARPEN_IS_WIN
@@ -29,13 +31,18 @@ namespace sharpen
     {
     private:
         using Self = sharpen::ExecuteContext;
+        
+        using Function = std::function<void()>;
     
         sharpen::NativeExecuteContextHandle handle_;
+        
+        static ExecuteContext InternalMakeContext(Function *entry);
     public:
       
         void Switch();
       
-        static ExecuteContext MakeContext();
+        template<typename _Fn,typename ..._Args>
+        static ExecuteContext MakeContext(_Fn &&fn,_Args &&...args);
         
   };
 }
