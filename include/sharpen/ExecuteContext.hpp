084251d256411b5a4cf1adf51143c67ed79ed804
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-09 13:50:05 +0800

    Update ExecuteContext.hpp

--- include/sharpen/ExecuteContext.hpp
+++ include/sharpen/ExecuteContext.hpp
@@ -2,8 +2,29 @@
 #ifndef _SHARPEN_EXECUTECONTEXT_HPP
 #define _SHARPEN_EXECUTECONTEXT_HPP
 
+#include "SystemMacro.hpp"
+
+#ifdef SHARPEN_IS_WIN
+
+#define SHARPEN_HAS_FIBER 
+#include <winbase.h>
+
+#else
+
+#define SHARPEN_HAS_UCONTEXT
+#include <ucontext.h>
+
+#endif
+
+
 namespace sharpen
 {
+#ifdef SHARPEN_HAS_FIBER
+  using NativeContextPtr = LPVOID;
+#else
+  using NativeContextPtr = ucontext*;
+#endif
+  
   class ExecuteContext;
 }
 
