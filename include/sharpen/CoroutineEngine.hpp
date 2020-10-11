5f527488a83369d486213bba23b0b0b9d5b8e602
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-11 16:19:44 +0800

    Update CoroutineEngine.hpp

--- include/sharpen/CoroutineEngine.hpp
+++ include/sharpen/CoroutineEngine.hpp
@@ -2,12 +2,21 @@
 #ifndef _SHARPEN_COROUTINEENGINE_HPP
 #define _SHARPEN_COROUTINEENGINE_HPP
 
-#include <thread>
+#include <memory>
 
 #include "ExecuteContext.hpp"
 
 namespace sharpen
 {
+  //one engine context per thread
+  //this pointer is nullptr by default
+  //if you use a coroutine function like Await()
+  //we will make some initializational operations
+  //it will call ConvertThreadToFiberEx and CreateFiberEx in windows and call getcontext and makecontext in *nix
+  extern thread_local std::unique_ptr<sharpen::ExecuteContext> LocalEngineContext;
+  
+  //this is a internal class and you should never use it directly
+  class CoroutineEngine;
 }
 
 #endif
