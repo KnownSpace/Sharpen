e504f1f557c4d6cfdcfec9f985ebbd5c5312a01d
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-11 16:51:39 +0800

    Update CoroutineEngine.hpp

--- include/sharpen/CoroutineEngine.hpp
+++ include/sharpen/CoroutineEngine.hpp
@@ -5,18 +5,49 @@
 #include <memory>
 
 #include "ExecuteContext.hpp"
+#include "Nonmovable.hpp"
+#include "BlockingQueue.hpp"
 
 namespace sharpen
 {
-  //one engine context per thread
-  //this pointer is nullptr by default
-  //if you use a coroutine function like Await()
-  //we will make some initializational operations
-  //it will call ConvertThreadToFiberEx and CreateFiberEx in windows and call getcontext and makecontext in *nix
-  extern thread_local std::unique_ptr<sharpen::ExecuteContext> LocalEngineContext;
+    //one engine context per thread
+    //this pointer is nullptr by default
+    //if you use a coroutine function like Await()
+    //we will make some initializational operations
+    //it will call ConvertThreadToFiberEx and CreateFiberEx in windows and call getcontext and makecontext in *nix
+    extern thread_local std::unique_ptr<sharpen::ExecuteContext> LocalEngineContext;
   
-  //this is a internal class and you should never use it directly
-  class CoroutineEngine;
+    //it is a internal class and you should never use it directly
+    class CoroutineEngine:public sharpen::Noncopyable,public sharpen::Nonmovable
+    {
+    private:
+        
+        void InternalPushTask(std::function<void()> &&fn);
+    public:
+        
+        //actually,we initializa LocalEngineContext in here
+        void LoadEngineContext();
+        
+        //if there are no any context in the queue
+        //we will block the thread
+        sharpen::ExecuteContext WaitContext();
+        
+        //push a context to the queue
+        void PushContext(sharpen::ExecuteContext &&context) noexcept;
+        
+        template<typename _Fn,typename ..._Args,typename = decltype(std::declval<_Fn>()(std::declval<_Args>()...))>
+        void PushTask(_Fn &&fn,_Args &&...args)
+        {
+            std::function<void()> func = std::bind(std::move(fn),args...);
+            this->InternalPushTask(std::move(func));
+        }
+    };
+  
+    extern sharpen::CoroutineEngine CentralEngine;
+  
+    //it is a internal function and you should not use it directly
+    //equal to while(true) WaitContext();
+    extern void CentralEngineLoopEntry();
 }
 
 #endif
