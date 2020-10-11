f06e5c226f58d35966e7254b35e6c5cc9680508a
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-11 17:20:40 +0800

    Update BlockingQueue.hpp

--- include/sharpen/BlockingQueue.hpp
+++ include/sharpen/BlockingQueue.hpp
@@ -2,9 +2,47 @@
 #ifndef _SHARPEN_BLOCKINGQUEUE_HPP
 #define _SHARPEN_BLOCKINGQUEUE_HPP
 
+#include <mutex>
+#include <condition_variable>
+#include <list>
+#include <type_trait>
+
+#include "Noncopyable.hpp"
+#include "Nonmovable.hpp"
+
 namespace sharpen
 {
-    class BlockingQueue;
+    template<typename _T>
+    class BlockingQueue:public sharpen::Noncopyable,public sharpen::Nonmovable
+    {
+    private:
+        using List = std::list<_T>;
+
+        std::mutex lock_;
+        std::condition_variable cond_;
+        List list_;
+    public:
+        void Push(_T object) noexcept
+        {
+            {
+                std::unique_lock lock(this->lock_);
+                this->list_.push_back(std::move(object));
+            }
+            this->cond_.notice_once();
+        }
+        
+        _T Pop() noexcept
+        {
+            std::unique_lock lock(this->lock_);
+            while(this->list_.empty())
+            {
+                this->cond_.wait(lock);
+            }
+            _T obj(std::move(this->list_.front()));
+            this->list_.pop_front();
+            return std::move(obj);
+        }
+    };
 }
 
 #endif
