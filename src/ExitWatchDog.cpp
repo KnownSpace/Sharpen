255771df0fd1a2d76ccc2ca9c5918a2ab6b97c44
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-11 17:47:57 +0800

    Update ExitWatchDog.cpp

--- src/ExitWatchDog.cpp
+++ src/ExitWatchDog.cpp
@@ -1,10 +1,19 @@
 #include <sharpen/ExitWatchDog.hpp>
 
-sharpen::ExitWatchDog::~ExitWatchDog()
+sharpen::ExitWatchDog::~ExitWatchDog() noexcept
 {
     if(sharpen::LocalEngineContext)
     {
+        this->ReleaseResource();
     }
+}
+
+void sharpen::ExitWatchDog::ReleaseResource() noexcept
+{
+#ifdef SHARPEN_HAS_FIBER
+    sharpen::LocalEngineContext.reset(nullptr);
+    ::ConvertFiberToThread();
+#endif
 }
 
 thread_local sharpen::LocalWatchDog;
