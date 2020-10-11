8e73cadea67e7e3cc55e7f08fe3b6a9121513624
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-11 16:29:13 +0800

    Create ExitWatchDog.hpp

---
+++ include/sharpen/ExitWatchDog.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef _SHARPEN_EXITWATCHDOG_HPP
+#define _SHARPEN_EXITWATCHDOG_HPP
+
+#include "Nonmovable.hpp"
+#include "ExecuteContext.hpp"
+
+namespace sharpen
+{
+    //it is a internal class and you should not use it directly
+    class ExitWatchDog:public sharpen::Noncopyable,public sharpen::Nonmovable
+    {
+    private:
+    public:
+        ExitWatchDog() = default;
+        
+        //we will free resource in here
+        ~ExitWatchDog() noexcept;
+    };
+    
+    //it release coroutine resource when thread exit
+    //we will do nothing if sharpen::LocalEngineContext is nullptr
+    extern thread_local ExitWatchDog LocalWatchDog;
+}
+
+#endif
