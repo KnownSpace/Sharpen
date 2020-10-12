47da438277018c61d1dbb617b8fdba86445ed705
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-13 00:01:36 +0800

    Update CoroutineEngine.cpp

--- src/CoroutineEngine.cpp
+++ src/CoroutineEngine.cpp
@@ -4,7 +4,23 @@ thread_local std::unique_ptr<sharpen::ExecuteContext> sharpen::LocalEngineContex
 
 thread_local std::unique_ptr<sharpen::ExecuteContext> sharpen::LocalFromContext(nullptr);
 
+sharpen::CoroutineEngine sharpen::CentralEngine;
+
 void sharpen::CouroutineEngine::InitThisThread()
 {
     sharpen::ExecuteContext::InternalEnableContextSwitch();
+}
+
+void sharpen::CentralEngineLoopEntry(void *lpNull)
+{
+    while(sharpen::CentralEngine.IsAlive())
+    {
+        if(sharpen::LocalFromContext)
+        {
+            sharpen::LocalFromContext->SetAutoRelease(true);
+            sharpen::CentralEngine.PushContext(std::move(sharpen::LocalFromContext));
+        }
+        std::unique_ptr<sharpen::ExecuteContext> ctx = std::move(sharpen::CentralEngine.WaitContext());
+        ctx->Switch(sharpen::LocalEngineContext);
+    }
 }
