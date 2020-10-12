39e172b2aee24208be139c3bfb82e9529e3f9fac
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-12 23:50:05 +0800

    Update CoroutineEngine.cpp

--- src/CoroutineEngine.cpp
+++ src/CoroutineEngine.cpp
@@ -3,3 +3,8 @@
 thread_local std::unique_ptr<sharpen::ExecuteContext> sharpen::LocalEngineContext(nullptr);
 
 thread_local std::unique_ptr<sharpen::ExecuteContext> sharpen::LocalFromContext(nullptr);
+
+void sharpen::CouroutineEngine::InitThisThread()
+{
+    sharpen::ExecuteContext::InternalEnableContextSwitch();
+}
