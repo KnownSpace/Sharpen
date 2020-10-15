6349fdf7051e894508dd7d1b9312b0f831d53e30
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-15 17:57:16 +0800

    Update AwaitableFuture.hpp

--- include/sharpen/AwaitableFuture.hpp
+++ include/sharpen/AwaitableFuture.hpp
@@ -1,11 +1,20 @@
 #pragma once
 #ifndef _SHARPEN_AWAITABLEFUTURE_HPP
+#define _SHARPEN_AWAITABLEFUTURE_HPP
 
+#include ”Awaiter.hpp"
 #include "Future.hpp"
+#include "CoroutineEngine.hpp"
 
 namespace sharpen
 {
-
+    template<typename _Result>
+    class AwaitableFuture:public sharpen::Future<_Result>
+    {
+    };
+  
+    template<typename _T>
+    using SharedAwaitableFuturePtr = std::shared_ptr<sharpen::AwaitableFuture<_T>>;
 }
 
 #endif
