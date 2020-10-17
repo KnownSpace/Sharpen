f72d546deef235a21e92918ca9b15c26deab822b
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-17 23:34:13 +0800

    Update BlockingQueue.hpp

--- include/sharpen/BlockingQueue.hpp
+++ include/sharpen/BlockingQueue.hpp
@@ -19,40 +19,71 @@ namespace sharpen
     {
     private:
         using List = std::list<_T>;
-        using Callback = std::function<void()>;
-        using CallbackList = std::list<Callback>
 
         std::mutex lock_;
         std::condition_variable cond_;
         List list_;
         sharpen::SpinLock subLock_;
+        bool subLocked_;
+        List pendingList_;
         
+        bool LockSub()
+        {
+            bool state = true;
+            std::swap(this->subLocked_,state);
+            return !state;
+        }
+        
+        List GetPending()
+        {
+            List pending;
+            std::swap(pending,this->pendingList_);
+            return pending;
+        }
+        
+        void UnlockSub()
+        {
+            this->subLocked_ = false;
+        }
     public:
         BlockingQueue()
         :lock_()
         ,cond_()
         ,list_()
-        ,asyncLock_()
+        ,subLock_()
+        ,subLocked(false)
+        ,pendingList_()
         {}
 
         void Push(_T object) noexcept
         {
+            //we push the object into pending list if cannot get the sub lock
+            bool getSubLock(false);
             {
-                std::unique_lock<std::mutex> lock(this->lock_);
-                this->list_.push_back(std::move(object));
+                std::unique_lock<sharpen::SpinLock> lock(this->subLock_);
+                getSubLock = this->LockSub();
+                if(!getSubLock)
+                {
+                    this->pendingList_.push_back(std::move(object));
+                    return;
+                }
             }
-            this->cond_.notice_once();
-        }
-        
-        void PushAsync(_T object) noexcept
-        {
-            _T *p = new _T(std::move(object));
-            assert(p != nullptr);
-            this->asyncLock_.Lock([this,p](){
-                std::unique_ptr<_T> obj(p);
-                this->Push(std::move(*obj));
-                this->asyncLock.Unlock();
-            });
+            //if we got the sub lock
+            {
+                    std::unique_lock<std::mutex> lock(this->lock_);
+                    this->list_.push_back(std::move(object));
+                    //get the lock again
+                    std::unique_lock<sharpen::SpinLock> subLock(this->subLock_);
+                    List &&pending = GetPending();
+                    //handle pending list
+                    for(auto begin = std::begin(pending),end = std::end(pending);begin != end;begin++)
+                    {
+                        this->list_.push_back(std::move(*begin));
+                    }
+                    this->UnlockSub();
+            }
+            //notice all threads
+            this->cond_.notice_all();
         }
         
         _T Pop() noexcept
