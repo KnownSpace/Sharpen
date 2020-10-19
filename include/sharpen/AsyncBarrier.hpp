f0052d2b68187d5bda3b1f2465c0edc3d646abfb
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-19 13:23:16 +0800

    Update AsyncBarrier.hpp

--- include/sharpen/AsyncBarrier.hpp
+++ include/sharpen/AsyncBarrier.hpp
@@ -3,18 +3,18 @@
 #define _SHARPEN_ASYNCBARRIER_HPP
 
 #include <list>
-#include <functional>
 
 #include "TypeDef.hpp"
-#include "Future.hpp"
+#include "AwaitableFuture.hpp"
 
 namespace sharpen
 {
     class AsyncBarrier:public sharpen::Noncopyable,public sharpen::Nonmovable
     {
     private:
-        using Function = std::function<void()>;
-        using List = std::list<Function>;
+        using MyFuture = sharpen::AwaitableFuture<void>;
+        using MyFuturePtr = MyFuture*;
+        using List = std::list<MyFuturePtr>;
 
         sharpen::Uint32 counter_;
         List waiters_;
@@ -23,13 +23,13 @@ namespace sharpen
     public:
         AsyncBarrier(sharpen::Uint32 count);
 
-        void Wait(Function &&callback);
+        void WaitAsync();
+        
+        void Notice() noexcept;
+        
+        void Reset();
 
-        sharpen::SharedFuturePtr<void> WaitAsync();
-
-        void Notice();
-
-        ~AsyncBarrier() = default;
+        ~AsyncBarrier() noexcept = default;
     };
     
 }
