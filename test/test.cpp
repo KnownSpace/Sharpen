f1cdc87edbb54b39f4d23a0edc6a2d8168be5aba
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-20 23:23:52 +0800

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,7 @@
 #include <sharpen/AwaitableFuture.hpp>
 #include <sharpen/AsyncOps.hpp>
 #include <thread>
+#include <ctime>
 
 #ifdef SHARPEN_IS_WIN
 
@@ -31,13 +32,34 @@ namespace sharpen
 
 #endif
 
-int main(int argc, char const *argv[])
+void LaunchTest()
 {
     for(size_t i = 0;i < 100000;++i)
     {
         sharpen::Launch([](){
-            std::printf("hello world\n");
+            //do nothing
         });
     }
+}
+
+void AwaitTest()
+{
+    for(size_t i = 0;i < 100000;i++)
+    {
+        sharpen::AwaitableFuture<void> future;
+        sharpen::Launch([&future](){
+            future.Complete();
+        });
+        future.Await();
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    std::clock_t begin,end;
+    begin = std::clock();
+    LaunchTest();
+    end = std::clock();
+    std::printf("using %d sec\n",end - begin);
     return 0;
 }
