fb9984b81841d2a6d3ffdf8563fc467c6532e1cc
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-20 13:10:00 +0800

    Update AsyncBarrier.cpp

--- src/AsyncBarrier.cpp
+++ src/AsyncBarrier.cpp
@@ -6,13 +6,39 @@ sharpen::AsyncBarrier::AsyncBarrier(sharpen::Uint32 counter)
     ,beginCounter_(counter)
 {}
 
-void sharpen::AsyncBarrier::Wait(sharpen::AsyncBarrier::Function &&callback)
+void sharpen::AsyncBarrier::WaitAsync()
+{
+    sharpen::AsyncBarrier::MyFuture future;
+    {
+        std::unique_lock<sharpen::SpinLock> lock(this->lock_);
+        if (this->counter_ == 0)
+        {
+            return;
+        }
+        this->waiters_.push_back(&future);
+    }
+    future.Await();
+}
+
+void sharpen::AsyncBarrier::Reset()
 {
     std::unique_lock<sharpen::SpinLock> lock(this->lock_);
-    if (this->counter_ == 0)
+    this->counter_ = this->beginCounter_;
+}
+
+void sharpen::AsyncBarrier::Notice() noexcept
+{
+    sharpen::AsyncBarrier::MyFuturePtr futurePtr;
     {
-        lock.unlock();
-        return;
+        std::unique_lock<sharpen::SpinLock> lock(this->lock_);
+        assert(this->counter_ != 0);
+        this->counter_ -= 1;
+        if(this->counter_ != 0)
+        {
+            return;
+        }
+        futurePtr = this->waiters_.front();
+        this->waiters_.pop_front();
     }
-    this->counter_ -= 1;
+    futurePtr->Complete();
 }
