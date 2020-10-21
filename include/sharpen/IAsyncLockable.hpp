35039fde9d56d19493681a43d4504fdf5ee6e488
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-21 18:12:58 +0800

    Update IAsyncLockable.hpp

--- include/sharpen/IAsyncLockable.hpp
+++ include/sharpen/IAsyncLockable.hpp
@@ -6,7 +6,32 @@ namespace sharpen
 {
     class IAsyncLockable
     {
-    
+    private:
+        using Self = sharpen::IAsyncLockable;
+    public:
+        IAsyncLockable() noexcept = default;
+        
+        IAsyncLockable(Self &&) noexcept = default;
+        
+        IAsyncLockable(const Self &) = default;
+        
+        virtual ~IAsyncLockable() = default;
+        
+        virtual void LockAsync() = 0;
+        
+        virtual void Unlock() noexcept = 0;
+        
+        //used by stl
+        inline void lock()
+        {
+            this->LockAsync();
+        }
+        
+        //used by stl
+        inline void unlock() noexcept
+        {
+            this->Unlock();
+        }
     };
 }
 
