96aa7c76e06f28e34ec36f033b47c0cc848cdc4c
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-29 13:29:06 +0800

    Create ObjectPool.hpp

---
+++ include/sharpen/ObjectPool.hpp
@@ -0,0 +1,109 @@
+#pragma once
+#ifndef _SHARPEN_OBJECTPOOL_HPP
+#define _SHARPEN_OBJECTPOOL_HPP
+
+#define <list>
+#include <functional>
+
+#include "Noncopyable.hpp"
+#include "TypeDef.hpp"
+
+namespace sharpen
+{
+    template<typename _T>
+    class ObjectPool:public sharpen::Noncopyable
+    {
+    private:
+        using Self = sharpen::ObjectPool<_T>;
+        using List = std::list<_T>;
+        using Creator = std::function<_T()>;
+        using Deletor = std::function<void(_T&)>;
+        
+        List objects_;
+        Creator creator_;
+        Deletor deletor_;
+        sharpen::Size maxReserved_;
+    public:
+        ObjectPool(Creator creator,sharpen::Size maxReserved)
+            :objects_()
+            ,creator_(std::move(creator))
+            ,deletor_()
+            ,maxReserved_(maxReserved)
+        {}
+        
+        ObjectPool(Creator creator,Deletor deletor，sharpen::Size maxReserved)
+            :objects_()
+            ,creator_(std::move(creator))
+            ,deletor_(std::move(deletor))
+            ,maxReserved_(maxReserved)
+        {}
+        
+        ObjectPool(Self &&other) noexcept
+            :objects_(std::move(other.objects_))
+            ,creator_(std::move(other.creator_))
+            ,deletor_(std::move(other.deletor_))
+            ,maxReserved_(other.maxReserved_)
+        {}
+        
+        ~ObjectPool() noexcept
+        {
+            for(auto begin = std::begin(this->objects_),end = std::end(this->objects_),begin != end;++begin)
+            {
+                this->deletor_(*begin);
+            }
+        }
+        
+        Self &operator=(Self &&other) noexcept
+        {
+            this->objects_ = std::move(other.objects_);
+            this->creator_ = std::move(other.creator_);
+            this->deletor_ = std::move(other.deletor_);
+            this->maxReserved = other.maxReserved_;
+            return *this;
+        }
+        
+        void Swap(Self &other) noexcept
+        {
+            std::swap(this->objects_,other.objects_);
+            std::swap(this->creator_,other.creator_);
+            std::swap(this->deletor_,other.deletor_);
+            std::swap(this->maxReserved_,other.maxReserved_);
+        }
+        
+        inline void swap(Self &other) noexcept
+        {
+            this->Swap(other);
+        }
+        
+        _T Get() noexcept
+        {
+            if(this->objects_.empty())
+            {
+                return std::move(this->creator_());
+            }
+            _T obj(std::move(this->objects_.front()));
+            this->objects_.pop_front();
+            return std::move(obj);
+        }
+        
+        void Reserve(sharpen::Size n)
+        {
+            for(sharpen::Size i = 0;i < n;++i)
+            {
+                this->objects_.push_back(std::move(this->creator()));
+            }
+        }
+        
+        void GiveBack(_T &&obj)
+        {
+            if(this->objects_.size() > this->maxReserved_)
+            {
+                this->deletor_(obj);
+                return;
+            }
+            this->objects_.push_back(std::move(obj));
+        }
+    };
+}
+
+#endif
