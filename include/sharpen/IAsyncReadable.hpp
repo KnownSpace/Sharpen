1eb083cc26b1635af7d76845d267eabf3b3c5479
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-30 12:37:29 +0800

    Create IAsyncReadable.hpp

---
+++ include/sharpen/IAsyncReadable.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef _SHARPEN_IASYNCREADABLE_HPP
+#define _SHARPEN_IASYNCREADABLE_HPP
+
+#include "ByteBuffer.hpp"
+#include "TypeDef.hpp"
+
+namespace sharpen
+{
+    class IAsyncReadable
+    {
+    private:
+        using Self = sharpen::IAsyncReadable;
+    public:
+        IAsyncReadable() = default;
+        
+        IAsyncReadable(const Self &) = default;
+        
+        IAsyncReadable(Self &&) noexcept = default;
+        
+        virtual ~IAsyncReadable() = default;
+        
+        virtual sharpen::Size ReadAsync(sharpen::Char *buf,sharpen::Size bufSize) = 0;
+        
+        virtual sharpen::Size ReadAsync(sharpen::ByteBuffer &buf) = 0;
+    };
+}
+
+#endif
