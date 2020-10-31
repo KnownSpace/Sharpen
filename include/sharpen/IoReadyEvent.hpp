6cfed2c683e049150876b2f87cf2b3d3116d669d
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-01 00:59:36 +0800

    Update IoReadyEvent.hpp

--- include/sharpen/IoReadyEvent.hpp
+++ include/sharpen/IoReadyEvent.hpp
@@ -31,12 +31,38 @@ namespace sharpen
         
         sharpen::FileHandle handle_;
         MyType type_;
+    protected:
+        virtual void HandleReadEvent(sharpen::ISelector &selector) = 0;
+        
+        virtual void HandleWriteEvent(sharpen::ISelector &selector) = 0;
+        
+        virtual void HandleCloseEvent(sharpen::ISelector &selector) = 0;
+        
+        virtual void HandleErrorEvent(sharpen::ISelector &selector) = 0;
     public:
         IoReadyEvent(sharpen::FileHandle handle,MyType type);
         
         ~IoReadyEvent() = default;
         
-        virtual void Handle(sharpen::ISelector &selector) override;
+        virtual void Handle(sharpen::ISelector &selector) override
+        {
+            if(this->type_ & MyType::Read)
+            {
+                this->HandleRead(selector);
+            }
+            if(this->type_ & MyType::Write)
+            {
+                this->HandleWrite(selector);
+            }
+            if(this->type_ & MyType::Close)
+            {
+                this->HandleClose(selector);
+            }
+            if(this->type_ & MyType::Error)
+            {
+                this->HandleError(selector);
+            }
+        }
     };
 }
 
