1e7711554ba6e0f372b84142920103837ba51793
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-01 00:06:09 +0800

    Create IoEvent.hpp

---
+++ include/sharpen/IoEvent.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef _SHARPEN_IOEVENT_HPP
+#define _SHARPEN_IOEVENT_HPP
+
+namespace sharpen
+{
+    class ISelector;
+
+    class IoEvent
+    {
+    private:
+        using Self = sharpen::IoEvent;
+    public:
+    
+        IoEvent() = default;
+        
+        IoEvent(const Self &) = default;
+        
+        IoEvent(Self &&) noexcept = default;
+        
+        virtual ~IoEvent() = default;
+        
+        virtual void Handle(ISelector &selector) = 0;
+    };
+}
+
+#endif
