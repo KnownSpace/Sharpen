66ec9d3b502959e43cfa37ab238f98425e0a5dfe
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-10-31 23:42:24 +0800

    Create EventFd.cpp

---
+++ src/EventFd.cpp
@@ -0,0 +1,59 @@
+#include <sharpen/EventFd.hpp>
+
+#ifdef SHARPEN_HAS_EVENTFD
+
+#include <sys/eventfd.h>
+#include <cassert>
+
+#include <sharpen/SystemError.hpp>
+
+sharpen::EventFd::EventFd(sharpen::Uint32 initVal,int flags)
+    :handle_(::eventfd(initVal,flags))
+{
+    if(handle_ == -1)
+    {
+        sharpen::ThrowLastError();
+    }
+}
+
+sharpen::EventFd::EventFd(sharpen::EventFd &&other) noexcept
+    :handle_(other.handle_)
+{
+    other.handle_ = -1;
+}
+
+sharpen::EventFd::~EventFd() noexcept
+{
+    if(this->handle_ != -1)
+    {
+        ::close(this->handle_);
+    }
+}
+
+sharpen::EventFd::EventFdValue sharpen::EventFd::Read()
+{
+    assert(this->handle_ != -1);
+    sharpen::EventFdValue val;
+    int r = ::read(this->handle_,&val,sizeof(val));
+    if(r == -1)
+    {
+        sharpen::ThrowLastError();
+    }
+    return val;
+}
+
+void sharpen::EventFd::Write(sharpen::EventFd::EventFdValue value)
+{
+    assert(this->handle_ != -1);
+    int r = ::write(this->handle_,&value,sizeof(value));
+    if(r == -1)
+    {
+        sharpen::ThrowLastError();
+    }
+}
+
+sharpen::FileHandle sharpen::EventFd::GetHandle() noexcept
+{
+    return this->handle_;
+}
+#endif
