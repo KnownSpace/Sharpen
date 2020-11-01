c76ac5c27f23728ee5f7abdc7ecb93c6644559c3
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-01 18:55:29 +0800

    Create IoCompletionPort.cpp

---
+++ src/IoCompletionPort.cpp
@@ -0,0 +1,57 @@
+#include <sharpen/IoCompletionPort.hpp>
+#ifdef SHARPEN_HAS_IOCP
+
+#include <cassert>
+
+#include <sharpen/SystemError.hpp>
+
+sharpen::IoCompletionPort::IoCompletionPort()
+    :handle_(::CreateIoCompletionPort(INVALID_HANDLE_VALUE,NULL,0,0))
+{
+    if(this->handle_ == NULL)
+    {
+        sharpen::ThrowLastError();
+    }
+}
+
+sharpen::IoCompletionPort::~IoCompletionPort() noexcept
+{
+    if(this->handle_ != NULL)
+    {
+        ::CloseHandle(this->handle_);
+    }
+}
+
+sharpen::Uint32 sharpen::IoCompletionPort::Wait(sharpen::IoCompletionPort::Event *events,sharpen::Uint32 maxEvent,sharpen::Uint32 timeout)
+{
+    assert(this->handle_ != NULL);
+    sharpen::Uint32 count = 0;
+    BOOL r = ::GetQueuedCompletionStatusEx(this->handle_,events,maxEvents,&count,timeout,FALSE);
+    if(r == FALSE)
+    {
+        sharpen::ThrowLastError();
+    }
+    return count;
+}
+
+void sharpen::IoCompletionPort::Bind(sharpen::FileHandle handle)
+{
+    assert(this->handle_ != NULL);
+    HANDLE r = ::CreateIoCompletionPort(handle,this->handle_,handle,0);
+    if(r == NULL)
+    {
+        sharpen::ThrowLastError();
+    }
+}
+
+void sharpen::IoCompletionPort::Post(sharpen::IoCompletionPort::Overlapped *overlapped,sharpen::Uint32 bytesTransferred,void *completionKey)
+{
+    assert(this->handle_ != NULL);
+    BOOL r = ::PostQueuedCompletionStatus(this->handle_,bytesTransferred,completionKey);
+    if(r == 0)
+    {
+        sharpen::ThrowLastError();
+    }
+}
+
+#endif
