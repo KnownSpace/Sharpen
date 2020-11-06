226f555f8128e57f00486feffd6af15be0ca3dc6
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-06 13:23:58 +0800

    Update EventLoop.hpp

--- include/sharpen/EventLoop.hpp
+++ include/sharpen/EventLoop.hpp
@@ -22,33 +22,56 @@ namespace sharpen
         using Task = std::function<void()>;
         using Lock = sharpen::SpinLock;
         using TaskVector = std::vector<Task>;
+        using TaskVectorPrr = std::shared_ptr<TaskVector>;
+        using LockPtr = std::shared_ptr<Lock>;
+        using SelectorPtr = std::shared_ptr<shared::ISelector>;
         
-        sharpen::ISelector *selector_;
-        TaskVector tasks_;
+        SelectorPtr selector_;
+        TaskVectorPtr tasks_;
         bool exectingTask_;
-        Lock lock_;
+        bool running_;
+        LockPtr lock_;
 
         //one loop per thread
         static thread_local EventLoop *LocalLoop;
+
+        //execute pending tasks
+        void ExecuteTask();
     public:
-        EventLoop(sharpen::ISelector *selector);
+        //create event loop with a selector and an uniqued task list
+        explicit EventLoop(SelectorPtr selector);
+        
+        //create event loop with a selector and an shared task list
+        EventLoop(SelectorPtr selector,TaskVectorPtr tasks,LockPtr lock);
         
         ~EventLoop() noexcept;
-              
+           
+        //bind a channel to event loop
+        //the channel must be supported by selector
         void Bind(sharpen::IChannel *channel);
         
+        //unbind a channel
         void Unbind(sharpen::IChannel *channel) noexcept;
         
+        //start listen write event on channel
+        //it will be ignored if IOCP is available
         void EnableWriteListen(sharpen::IChannel *channel);
         
+        //stop listen write event on channel
+        //it will be ignored if IOCP is available
         void DisableWriteListen(sharpen::IChannel *channel);
         
+        //queue a task to event loop
+        //the task will be executed in next loop
         void QueueInLoop(Task task);
         
+        //run event loop in this thread
         void Run();
         
+        //stop event loop
         void Stop();
 
+        //get thread local event loop
         static sharpen::EventLoop *GetLocalLoop() noexcept;
     };
 }
