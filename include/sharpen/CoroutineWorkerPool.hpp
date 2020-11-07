05046044cc7ffd624c03dca856323cc06f136be4
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-07 23:43:21 +0800

    Create CoroutineWorkerPool.hpp

---
+++ include/sharpen/CoroutineWorkerPool.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef _SHARPEN_COROUTINEWORKERPOOL_HPP
+#define _SHARPEN_COROUTINEWORKERPOOL_HPP
+
+#include <thread>
+#include <vector>
+#include <memory>
+
+#include "TypeDef.hpp"
+#include "CoroutineWorker.hpp"
+#include "Noncopyable.hpp"
+#include "Nonmovable.hpp"
+
+namespace sharpen
+{
+    class CoroutineWorkerPool
+    {
+    private:
+        using ThreadPtr = std::unique_ptr<std::thread>;
+        using WorkerPtr = std::unique_ptr<sharpen::CoroutineWorker>;
+        using ThreadVector = std::vector<ThreadPtr>;
+        using WorkerVector = std::vector<WorkerPtr>;
+        
+        WorkerVector workers_;
+        ThreadVector threads_;
+    public:
+        explicit CoroutineWorkerPool(sharpen::Size poolSize);
+        
+        ~CoroutineWorkerPool() noexcept;
+        
+        void Stop();
+    };
+}
+
+#endif
