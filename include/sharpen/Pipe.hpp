47d0abed76725ecf175727cbd8d6418d32d4f306
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-15 16:33:47 +0800

    Create Pipe.hpp

---
+++ include/sharpen/Pipe.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _SHARPEN_PIPE_HPP
+#define _SHARPEN_PIPE_HPP
+
+#include "SystemMacro.hpp"
+#include "Noncopyable.hpp"
+#include "Nonmovable.hpp"
+#include "FileTypeDef.hpp"
+#include "TypeDef.hpp"
+
+namespace sharpen
+{
+    class Pipe:public sharpen::Noncopyable,public sharpen::Nonmovable
+    {
+    private:
+    public:
+    };
+}
+
+#endif
