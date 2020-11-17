0072d01a29f9d6b3b03c8aa1bef7d25a205d1e6d
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-18 00:14:39 +0800

    Update Kqueue.cpp

--- src/Kqueue.cpp
+++ src/Kqueue.cpp
@@ -1,4 +1,48 @@
 #include <sharpen/Kqueue.hpp>
 #ifdef SHARPEN_HAS_KQUEUE
-//todo
+
+#include <sharpen/SystemError.hpp>
+
+sharpen::Kqueue::Kqueue()
+    :handle_(::kqueue())
+{
+    if(this->handle_ == -1)
+    {
+        sharpen::ThrowLastError();
+    }
+}
+
+~sharpen::Kqueue::Kqueue() noexcept
+{
+    ::close(this->handle_);
+}  
+
+sharpen::Uint32 sharpen::Kqueue::Wait(sharpen::Kqueue::Event *events,sharpen::Int32 maxEvent,int timeout)
+{
+    timespec *timeoutPtr = nullptr;
+    timespec timeoutSpec;
+    if(timeout != -1)
+    {
+        time_t sec = timeout / 1000);
+        timeout %= 1000;
+        timeoutSpec.tv_sec = sec;
+        timeoutSpec.tv_nsec = timeout * 1000 * 1000;
+        timeoutPtr = &timeoutSpec;
+    }
+    sharpen::Int32 r = ::kevent(this->handle_,nullptr,0,events,maxEvent,timeoutPtr);
+    if(r == -1)
+    {
+        sharpen::ThrowLastError();
+    }
+    return r;
+}
+
+void sharpen::Kqueue::Add(sharpen::FileHandle handle,sharpen::Int16 eventType,void *data)
+{}
+
+void sharpen::Kqueue::Remove(sharpen::FileHandle handle)
+{}
+
+void sharpen::Kqueue::Update(sharpen::FileHandle handle,sharpen::Int16 eventType,void *data)
+{}
 #endif
