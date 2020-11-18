721b589b3a9883c3312625cadf461387e8e7db63
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-11-18 18:37:37 +0800

    Create TimerQueue.hpp

---
+++ include/sharpen/TimerQueue.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _SHARPEN_TIMERQUEUE
+#define _SHARPEN_TIMERQUEUE
+
+#include "Noncopyable.hpp"
+
+namespace sharpen
+{
+    class TimerQueue:public sharpen::Noncopyable
+    {};
+}
+
+#endif
