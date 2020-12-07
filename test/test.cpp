bfc630b786d611d36cfa8eb6c91daa47dc35b617
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-12-07 18:00:56 +0800

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <sharpen/AwaitableFuture.hpp>
 #include <sharpen/AsyncOps.hpp>
+#include <sharpen/AsyncMutex.hpp>
 #include <thread>
 #include <ctime>
+#include <mutex>
+#include <string>
 
 #ifdef SHARPEN_IS_WIN
 
@@ -76,13 +79,36 @@ void MultithreadLaunchTest()
     std::printf("LaunchTest using %d sec in thread %d\n",time,sharpen::GetCurrentThreadId());
 }
 
+void MutexTest(sharpen::AsyncMutex &lock,int &count)
+{
+    std::unique_lock<sharpen::AsyncMutex> _lock(lock);
+    count++;
+    std::printf("count is %d\n");
+}
+
 int main(int argc, char const *argv[])
 {
     std::printf("running in machine with %d cores\n",std::thread::hardware_concurrency());
     //multithreaded await test
-    std::thread t1(std::bind(&MultithreadAwaitTest)),t2(std::bind(&MultithreadAwaitTest));
-    t1.join();
-    t2.join();
+    std::string arg("basic");
+    if(argc > 1)
+    {
+        arg = argv[1];
+    }
+    if(arg == "basic")
+    {
+        std::thread t1(std::bind(&MultithreadAwaitTest)),t2(std::bind(&MultithreadAwaitTest));
+        t1.join();
+        t2.join();
+    }
+    if(arg == "mutex")
+    {
+        int count = 0;
+        sharpen::AsyncMutex lock;
+        std::thread t1(std::bind(&MutexTest,lock,count)),t2(std::bind(&MutexTest,lock,count));
+        t1.join();
+        t2.join();
+    }
     std::printf("test complete\n");
     return 0;
 }
