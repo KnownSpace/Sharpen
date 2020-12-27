9e6b5fa7b37a3c1b1170796c2684baa5a917c7f9
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-12-27 16:51:17 +0800

    Create WorkerPool.hpp

---
+++ include/sharpen/WorkerPool.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef _SHARPEN_WORKERPOOL_HPP
+#define _SHARPEN_WORKERPOOL_HPP
+
+#include <thread>
+
+#include "AsyncSemaphore.hpp"
+#include "AsyncBarrier.hpp"
+
+namespace sharpen
+{
+    class WorkerPool:public sharpen::Noncopyable,public sharpen::Nonmovable
+    {
+    private:
+        
+        //running
+        bool running_;
+        //worker count
+        sharpen::Size count_;
+        //stop flag
+        sharpen::AsyncBarrier stopFlag_;
+        //thread flag
+        sharpen::AsyncSemaphore threadFlag_;
+    public:
+    
+        WorkerPool(sharpen::Size count);
+        
+        ~WorkerPool() noexcept;
+        
+        //start working
+        void Start();
+        
+        //stop all worker
+        void Stop() noexcept;
+        
+        //get worker count
+        sharpen::Size GetWorkerCount() const noexcept;
+        
+        //set worker count
+        void SetWorkerCount(sharpen::Size count);
+    };
+}
+
+#endif
