bcdaf10951b0333082847a57edbec0c8749576a5
孜然 <2145973003@qq.com>
2021-03-28 11:51:47 +0800

    update

--- src/EventEngine.cpp
+++ src/EventEngine.cpp
@@ -15,13 +15,15 @@ sharpen::EventEngine::EventEngine(sharpen::Size workerCount)
     for (size_t i = 0; i < workerCount; i++)
     {
 #ifdef SHARPEN_IS_WIN
+        //global selector
         sharpen::SelectorPtr selector = sharpen::MakeDefaultSelector();
         auto tasks = std::make_shared<std::vector<std::function<void()>>>();
         auto lock = std::make_shared<sharpen::SpinLock>();
         std::unique_ptr<sharpen::EventLoopThread> thread(new sharpen::EventLoopThread(selector,tasks,lock));
         this->workers_.push_back(std::move(thread));
 #else
-        std::unique_ptr<sharpen::EventLoopThread> thread(sharpen::MakeDefaultSelector());
+        //one selector per thread
+        std::unique_ptr<sharpen::EventLoopThread> thread(new sharpen::EventLoopThread(sharpen::MakeDefaultSelector()));
         this->workers_.push_back(std::move(thread));
 #endif
     }
