eeae1e7d7c262044c982975c0e08ed16e3ce8af3
NaturalSharp <2145973003@qq.com>
2021-07-20 23:48:15 +0800

    [Fix]fix include

--- include/sharpen/CompilerInfo.hpp
+++ include/sharpen/CompilerInfo.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#ifndef _SHARPEN_COMPILERINFO_HPP
+#define _SHARPEN_COMPILERINFO_HPP
 #ifdef _MSC_VER
 #define SHARPEN_COMPILER_MSVC
 #elif defined(__GUNC__)
 #define SHARPEN_COMPILER_GCC
 #elif defined(__clang_)
 #define SHARPEN_COMPILER_CLANG
+#endif
 #endif
