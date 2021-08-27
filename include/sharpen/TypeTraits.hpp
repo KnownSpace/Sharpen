b1a3519d28ece42be502041c9ee541fae92190cb
NaturalSharp <2145973003@qq.com>
2021-08-27 16:22:11 +0800

    [Add]more powerful IsMatches

--- include/sharpen/TypeTraits.hpp
+++ include/sharpen/TypeTraits.hpp
@@ -57,47 +57,32 @@ namespace sharpen
         }
     };
 
-    template<template<class> class _Checker>
-    struct ValidTemplateContainer
-    {
-    private:
-        template<typename _Arg,typename _Check = decltype(std::declval<_Checker<_Arg>>())>
-        constexpr sharpen::TrueType Test(int) noexcept
-        {
-            return sharpen::TrueType{};
-        }
-
-        template<typename _Arg>
-        constexpr sharpen::FalseType Test(...) noexcept
-        {
-            return sharpen::FalseType{};
-        }
-    public:
-
-        template<typename _Arg>
-        constexpr auto operator()(_Arg &&arg) noexcept -> decltype(Test<_Arg>(0))
-        {
-            return Test<_Arg>(0);
-        }
-    };
-
     template<typename _Check>
     constexpr sharpen::ValidContainer<_Check> InternalIsValid() noexcept
     {
         return sharpen::ValidContainer<_Check>();
     }
 
-    template<template<class> class _Check>
-    constexpr sharpen::ValidTemplateContainer<_Check> InternalIsTemplateValid() noexcept
-    {
-        return sharpen::ValidTemplateContainer<_Check>();
-    }
-
     template<typename _Check,typename _T>
     using IsValid = decltype(sharpen::InternalIsValid<_Check>()(std::declval<_T>()));
 
-    template<template<class> class _Check,typename _T>
-    using IsMatches = decltype(sharpen::InternalIsTemplateValid<_Check>()(std::declval<_T>()));
+    struct MatchesContainer
+    {
+        template<template<class ...> class _Tmp,typename ..._T,typename _Check = _Tmp<_T...>>
+        constexpr static sharpen::TrueType Matches(int) noexcept
+        {
+            return sharpen::TrueType();
+        }
+
+        template<template<class ...> class _Tmp,typename ..._T>
+        constexpr static sharpen::FalseType Matches(...) noexcept
+        {
+            return sharpen::FalseType();
+        }
+    };
+    
+    template<template<class ...> class _Tmp,typename ..._T>
+    using IsMatches = decltype(sharpen::MatchesContainer::Matches<_Tmp,_T...>(0));
 }
 
 #endif
