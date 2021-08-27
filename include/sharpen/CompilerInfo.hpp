b8c8b1e2b9e9e8b3dc40a5a8171239572d977cdf
NaturalSharp <2145973003@qq.com>
2021-08-27 13:53:21 +0800

    [Fix]try fix error

--- include/sharpen/CompilerInfo.hpp
+++ include/sharpen/CompilerInfo.hpp
@@ -4,9 +4,11 @@
 #define _SHARPEN_COMPILERINFO_HPP
 #ifdef _MSC_VER
 #define SHARPEN_COMPILER_MSVC
-#elif defined(__GUNC__)
+#endif
+#ifdef __GUNC__
 #define SHARPEN_COMPILER_GCC
-#elif defined(__clang_)
+#endif
+#ifdef __clang_
 #define SHARPEN_COMPILER_CLANG
 #endif
 #endif
