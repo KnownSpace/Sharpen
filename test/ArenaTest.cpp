35ba9e94fe49b391aa0c5a392066c8dbab8e0d65
NaturalSharp <2145973003@qq.com>
2021-11-04 16:14:25 +0800

    [Update]add benchmark

--- test/ArenaTest.cpp
+++ test/ArenaTest.cpp
@@ -1,14 +1,16 @@
 #include <sharpen/Arena.hpp>
 #include <cassert>
 #include <cstdio>
+#include <sharpen/StopWatcher.hpp>
 
 class MyTestClass
 {
 private:
     int *p_;
+
 public:
     MyTestClass(int *p)
-        :p_(p)
+        : p_(p)
     {
         *p += 1;
     }
@@ -32,7 +34,7 @@ int main(int argc, char const *argv[])
     std::puts("testing unique array");
     {
         sharpen::Arena arena;
-        auto array = arena.MakeUniqueArray<MyTestClass>(10,&p);
+        auto array = arena.MakeUniqueArray<MyTestClass>(10, &p);
     }
     assert(p == 0);
     std::puts("testing shared object");
@@ -45,9 +47,32 @@ int main(int argc, char const *argv[])
     std::puts("testing shared array");
     {
         sharpen::Arena arena;
-        auto obj = arena.MakeSharedArray<MyTestClass>(10,&p);
+        auto obj = arena.MakeSharedArray<MyTestClass>(10, &p);
         auto copy = obj;
     }
     std::puts("pass");
+    std::puts("benchmark");
+    constexpr size_t count = static_cast<size_t>(1e8);
+    sharpen::StopWatcher sw;
+    sw.Begin();
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            std::unique_ptr<size_t> p(new size_t(i));
+        }
+    }
+    sw.Stop();
+    std::printf("malloc using %zu tu\n",static_cast<size_t>(sw.Compute()));
+    sw.Begin();
+    {
+        sharpen::Arena arena;
+        for (size_t i = 0; i < count; i++)
+        {
+            auto p = arena.MakeUniqueObject<size_t>(i);
+        }
+    }
+    sw.Stop();
+    std::printf("arena using %zu tu\n",static_cast<size_t>(sw.Compute()));
+    std::printf("1 sec = %zu tu\n",static_cast<size_t>(CLOCKS_PER_SEC));
     return 0;
 }
