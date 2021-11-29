c0057ea30615528c0b0a57c7a63f060067c97859
NaturalSharp <2145973003@qq.com>
2021-11-29 19:03:24 +0800

    [Add]cancelable named requires

--- include/sharpen/QuorumConcepts.hpp
+++ include/sharpen/QuorumConcepts.hpp
@@ -8,7 +8,8 @@
 namespace sharpen
 {
     template<typename _Proposer,typename _Proposal>
-    using InternalIsQuorumProposer = auto(*)()->decltype(sharpen::DeclLvalue<_Proposer>().ProposeAsync(std::declval<_Proposal>(),std::declval<sharpen::Future<bool>&>()));
+    using InternalIsQuorumProposer = auto(*)()->decltype(sharpen::DeclLvalue<_Proposer>().ProposeAsync(std::declval<_Proposal>()
+                                                        ,std::declval<sharpen::Future<bool>&>()));
 
     template<typename _Proposer,typename _Proposal>
     using IsQuorumProposer = sharpen::IsMatches<sharpen::InternalIsQuorumProposer,_Proposer,_Proposal>;
@@ -18,6 +19,18 @@ namespace sharpen
 
     template<typename _ProposerMapIterator,typename _Proposal>
     using IsQuorumProposerMapIterator = sharpen::IsQuorumProposer<decltype(*std::declval<_ProposerMapIterator>()->second),_Proposal>;
+
+    template<typename _Proposer>
+    using InternalCancelableQuorumProposer = auto(*)() ->decltype(std::declval<_Proposer>().Cancel());
+
+    template<typename _Proposer,typename _Proposal>
+    using IsCancelableQuorumProposer = sharpen::BoolType<sharpen::IsQuorumProposer<_Proposer,_Proposal>::Value && sharpen::IsMatches<sharpen::InternalCancelableQuorumProposer,_Proposer>::Value>;
+
+    template<typename _ProposerIterator,typename _Proposal>
+    using IsCancelableQuorumProposerIterator = sharpen::IsCancelableQuorumProposer<decltype(*std::declval<_ProposerIterator>()),_Proposal>;
+
+    template<typename _ProposerMapIterator,typename _Proposal>
+    using IsCancelableQuorumProposerMapIterator = sharpen::IsCancelableQuorumProposer<decltype(*std::declval<_ProposerMapIterator>()->second),_Proposal>;
 }
 
 #endif
