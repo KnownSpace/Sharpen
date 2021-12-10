4be2c72002221c094b2fa42341f42f3c241ec953
NaturalSharp <2145973003@qq.com>
2021-12-11 00:11:27 +0800

    [Update]update test

--- test/ChecksumTest.cpp
+++ test/ChecksumTest.cpp
@@ -7,11 +7,11 @@ int main(int argc, char const *argv[])
     std::puts("crc16-modbus test begin");
     char buf[] = "123456789";
     sharpen::Uint32 checksum = sharpen::Crc16(buf,sizeof(buf) - 1);
-    std::printf("check sum is %x\n",checksum);
+    std::printf("check sum is %X\n",checksum);
     assert(checksum == 0x4B37);
     std::puts("pass");
     checksum = sharpen::Adler32(buf,sizeof(buf) - 1);
-    std::printf("check sum is %x\n",checksum);
+    std::printf("check sum is %X\n",checksum);
     assert(checksum == 0x91E01DE);
     std::puts("pass");
     return 0;
