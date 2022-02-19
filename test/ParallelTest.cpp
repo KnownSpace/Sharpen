c5120fbd96bd76221f5bf62d1587df66200f5cb3
NaturalSharp <2145973003@qq.com>
2022-02-19 23:13:53 +0800

    [Update]update test

--- test/ParallelTest.cpp
+++ test/ParallelTest.cpp
@@ -3,28 +3,25 @@
 
 void ParallelTest(size_t n)
 {
-    sharpen::EventEngine &engine = sharpen::EventEngine::SetupEngine();
-    engine.Startup([n]()
-    {
-        std::printf("parallel test begin\n");
-        std::atomic_size_t ar{0};
-        sharpen::ParallelFor(0,n,[&ar](size_t i)
-        {
-            ar.fetch_add(i);
-        });
-        size_t r{0};
-        for (size_t i = 0; i < n; i++)
-        {
-            r += i;
-        }
-        std::printf("r is %zu ar is %zu\n",r,ar.load());
-        assert(r == ar);
-        std::printf("parallel test pass\n");
+    std::printf("parallel test begin\n");
+    std::atomic_size_t ar{0};
+    sharpen::ParallelFor(0, n, [&ar](size_t i)
+    { 
+        ar.fetch_add(i); 
     });
+    size_t r{0};
+    for (size_t i = 0; i < n; i++)
+    {
+        r += i;
+    }
+    std::printf("r is %zu ar is %zu\n", r, ar.load());
+    assert(r == ar);
+    std::printf("parallel test pass\n");
 }
 
 int main()
 {
-    ParallelTest(static_cast<sharpen::Size>(10e3));
+    sharpen::EventEngine &engine = sharpen::EventEngine::SetupEngine();
+    engine.Startup(&ParallelTest,static_cast<sharpen::Size>(1e4));
     return 0;
 }
