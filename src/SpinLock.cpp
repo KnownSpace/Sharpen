dbac9adf9c9bfc0ee16bb4d2a3324eab89d987b9
NaturalSharp <2145973003@qq.com>
2022-03-25 18:46:15 +0800

    [Refactor]refactor spin lock

--- src/SpinLock.cpp
+++ src/SpinLock.cpp
@@ -2,14 +2,36 @@
 
 #include <thread>
 
+#include <sharpen/CompilerInfo.hpp>
+#include <sharpen/TypeDef.hpp>
+
+#ifdef SHARPEN_COMPILER_MSVC
+#define SHARPEN_PAUSE
+#else
+#if (defined __x86_64__ || defined __x86_64 || defined __x86__ || defined __x86)
+#define SHARPEN_PAUSE asm("pause")
+#else
+#define SHARPEN_PAUSE
+#endif
+#endif
+
 sharpen::SpinLock::SpinLock()
     :flag_()
 {}
 
 void sharpen::SpinLock::lock()
 {
+    sharpen::Size i{0};
     while (this->flag_.test_and_set(std::memory_order::memory_order_acquire))
-    {}
+    {
+        SHARPEN_PAUSE;
+        ++i;
+        if(i == 256)
+        {
+            std::this_thread::yield();
+            i = 0;
+        }
+    }
 }
 
 void sharpen::SpinLock::unlock() noexcept
@@ -21,3 +43,7 @@ bool sharpen::SpinLock::TryLock()
 {
     return !this->flag_.test_and_set(std::memory_order::memory_order_acquire);
 }
+
+#ifdef SHARPEN_PAUSE
+#undef SHARPEN_PAUSE
+#endif
