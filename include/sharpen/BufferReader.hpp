2dc4ea559dfec7bd15b0b430da00f3298cbfebfc
NaturalSharp <2145973003@qq.com>
2022-11-03 16:11:01 +0800

    update

---
+++ include/sharpen/BufferReader.hpp
@@ -0,0 +1,100 @@
+#pragma once
+#ifndef _SHARPEN_BUFFERREADER_HPP
+#define _SHARPEN_BUFFERREADER_HPP
+
+#include <cassert>
+
+#include "Noncopyable.hpp"
+#include "ByteBuffer.hpp"
+#include "BinarySerializator.hpp"
+
+namespace sharpen
+{
+    class BufferReader:public sharpen::Noncopyable
+    {
+    private:
+        using Self = sharpen::BufferReader;
+    
+        std::size_t offset_;
+        sharpen::ByteBuffer *target_;
+    public:
+    
+        explicit BufferReader(sharpen::ByteBuffer *target)
+            :offset_(0)
+            ,target_(target)
+        {
+            assert(this->target_);
+        }
+    
+        BufferReader(Self &&other) noexcept
+            :offset_(other.offset_)
+            ,target_(other.target_)
+        {
+            other.offset_ = 0;
+            other.target_ = nullptr;
+        }
+    
+        inline Self &operator=(Self &&other) noexcept
+        {
+            if(this != std::addressof(other))
+            {
+                this->offset_ = other.offset_;
+                this->target_ = other.target_;
+                other.offset_ = 0;
+                other.target_ = nullptr;
+            }
+            return *this;
+        }
+    
+        ~BufferReader() noexcept = default;
+    
+        inline const Self &Const() const noexcept
+        {
+            return *this;
+        }
+
+        inline bool Readable() const noexcept
+        {
+            return this->target_;
+        }
+
+        inline sharpen::ByteBuffer &Target() noexcept
+        {
+            assert(this->target_);
+            return *this->target_;
+        }
+
+        inline const sharpen::ByteBuffer &Target() const noexcept
+        {
+            assert(this->target_);
+            return *this->target_;
+        }
+
+        template<typename _T,typename _Check = decltype(sharpen::BinarySerializator::LoadFrom(std::declval<_T&>(),nullptr,0))>
+        inline void Read(_T &obj) const
+        {
+            assert(this->target_);
+            std::size_t sz{this->target_->GetSize() - this->offset_};
+            if(!sz)
+            {
+                throw std::out_of_range{"index out of range"};
+            }
+            this->offset_ += sharpen::BinarySerializator::LoadFrom(obj,this->target_->Data(),sz);
+        }
+
+        template<typename _T,typename _Check = decltype(sharpen::BinarySerializator::LoadFrom(std::declval<_T&>(),nullptr,0),_T{})>
+        inline _T Read() const
+        {
+            _T obj{};
+            this->Read(obj);
+            return obj;
+        }
+
+        inline std::size_t GetOffset() const noexcept
+        {
+            return this->offset_;
+        }
+    };
+}
+
+#endif
---
+++ include/sharpen/BufferWriter.hpp
@@ -0,0 +1,98 @@
+#pragma once
+#ifndef _SHARPEN_BUFFERWRITER_HPP
+#define _SHARPEN_BUFFERWRITER_HPP
+
+#include <cassert>
+
+#include "Noncopyable.hpp"
+#include "ByteBuffer.hpp"
+#include "BinarySerializator.hpp"
+
+namespace sharpen
+{
+    class BufferWriter:public sharpen::Noncopyable
+    {
+    private:
+        using Self = sharpen::BufferWriter;
+    
+        std::size_t offset_;
+        sharpen::ByteBuffer *target_;
+    public:
+    
+        explicit BufferWriter(sharpen::ByteBuffer *target)
+            :offset_(0)
+            ,target_(target)
+        {
+            assert(this->target_);
+        }
+    
+        BufferWriter(Self &&other) noexcept
+            :offset_(other.offset_)
+            ,target_(other.target_)
+        {
+            other.offset_ = 0;
+            other.target_ = nullptr;
+        }
+    
+        inline Self &operator=(Self &&other) noexcept
+        {
+            if(this != std::addressof(other))
+            {
+                this->offset_ = other.offset_;
+                this->target_ = other.target_;
+                other.offset_ = 0;
+                other.target_ = nullptr;
+            }
+            return *this;
+        }
+    
+        ~BufferWriter() noexcept = default;
+    
+        inline const Self &Const() const noexcept
+        {
+            return *this;
+        }
+
+        inline bool Writable() const noexcept
+        {
+            return this->target_;
+        }
+
+        inline sharpen::ByteBuffer &Target() noexcept
+        {
+            assert(this->target_);
+            return *this->target_;
+        }
+
+        inline const sharpen::ByteBuffer &Target() const noexcept
+        {
+            assert(this->target_);
+            return *this->target_;
+        }
+
+        template<typename _T,typename _Check = decltype(sharpen::BinarySerializator::UnsafeStoreTo(std::declval<const _T&>(),nullptr),sharpen::BinarySerializator::ComputeSize(std::declval<const _T&>()))>
+        inline void Write(const _T &obj)
+        {
+            assert(this->target_);
+            std::size_t sz{sharpen::BinarySerializator::ComputeSize(obj)};
+            if(this->target_->GetSize() < this->offset_ + sz)
+            {
+                this->target_->ExtendTo(this->offset_ + sz);
+            }
+            this->offset_ += sharpen::BinarySerializator::UnsafeStoreTo(obj,this->target_->Data() + this->offset_);
+        }
+
+        inline std::size_t GetLength() const noexcept
+        {
+            return this->offset_;
+        }
+
+        inline void Resize(std::size_t size)
+        {
+            assert(this->target_);
+            this->target_->ExtendTo(size);
+        }
+    };
+}
+
+#endif
