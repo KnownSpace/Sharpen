6c9dfecbafe2fc453642a3b3a532e67a7d3c167e
NaturalSharp <2145973003@qq.com>
2023-01-06 19:13:56 +0800

    Add GenericMail class

---
+++ include/sharpen/GenericMail.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#ifndef _SHARPEN_GENERICMAIL_HPP
+#define _SHARPEN_GENERICMAIL_HPP
+
+#include "Mail.hpp"
+#include "GenericMailHeader.hpp"
+#include "TypeTraits.hpp"
+
+namespace sharpen
+{
+    class GenericMail:private sharpen::Mail
+    {
+    private:
+        using Self = sharpen::GenericMail;
+        using Base = sharpen::Mail;
+    
+        sharpen::GenericMailHeader &GenericHeader() noexcept;
+
+        const sharpen::GenericMailHeader &GenericHeader() const noexcept;
+    public:
+
+        GenericMail() noexcept;
+    
+        explicit GenericMail(std::uint32_t magic) noexcept;
+    
+        GenericMail(const Self &other) = default;
+    
+        GenericMail(Self &&other) noexcept = default;
+    
+        Self &operator=(const Self &other) = default;
+    
+        Self &operator=(Self &&other) noexcept = default;
+    
+        ~GenericMail() noexcept = default;
+    
+        inline const Self &Const() const noexcept
+        {
+            return *this;
+        }
+
+        template<typename _T,typename _Check = sharpen::EnableIf<std::is_standard_layout<_T>::value && sizeof(_T) == sharpen::GenericMailHeader::formSize>>
+        inline _T &Form() noexcept
+        {
+            return this->GenericHeader().Form<_T>();
+        }
+
+        template<typename _T,typename _Check = sharpen::EnableIf<std::is_standard_layout<_T>::value && sizeof(_T) == sharpen::GenericMailHeader::formSize>>
+        inline const _T &Form() const noexcept
+        {
+            return this->GenericHeader().Form<_T>();
+        }
+
+        std::uint32_t GetMagic() const noexcept;
+
+        void SetMagic(std::uint32_t magic) noexcept;
+
+        inline const sharpen::ByteBuffer &ConstContent() const noexcept
+        {
+            return Base::Content();
+        }
+
+        void SetContent(sharpen::ByteBuffer content);
+    };
+}
+
+#endif
---
+++ src/GenericMail.cpp
@@ -0,0 +1,44 @@
+#include <sharpen/GenericMail.hpp>
+
+#include <sharpen/IntOps.hpp>
+
+sharpen::GenericMail::GenericMail()
+    :Self(0)
+{}
+
+sharpen::GenericMail::GenericMail(std::uint32_t magic)
+    :Base()
+{
+    this->Header().ExtendTo(sizeof(sharpen::GenericMailHeader));
+    this->Header().As<sharpen::GenericMailHeader>().SetMagic(magic);
+}
+
+sharpen::GenericMailHeader &sharpen::GenericMail::GenericHeader() noexcept
+{
+    assert(this->Header().GetSize() == sizeof(sharpen::GenericMailHeader));
+    return this->Header().As<sharpen::GenericMailHeader>();
+}
+
+const sharpen::GenericMailHeader &sharpen::GenericMail::GenericHeader() const noexcept
+{
+    assert(this->Header().GetSize() == sizeof(sharpen::GenericMailHeader));
+    return this->Header().As<sharpen::GenericMailHeader>();
+}
+
+std::uint32_t sharpen::GenericMail::GetMagic() const noexcept
+{
+    return this->GenericHeader().GetMagic();
+}
+
+void sharpen::GenericMail::SetMagic(std::uint32_t magic) noexcept
+{
+    this->GenericHeader().SetMagic(magic);
+}
+
+void sharpen::GenericMail::SetContent(sharpen::ByteBuffer content)
+{
+    //overflow check
+    std::uint32_t contentSize{sharpen::IntCast<std::uint32_t>(content.GetSize())};
+    this->GenericHeader().SetContentSize(contentSize);
+    Base::Content() = std::move(content);
+}
