492697f5091e717c0b20b6fcc867886885656951
NaturalSharp <2145973003@qq.com>
2023-01-10 21:43:49 +0800

    Fix bug

--- src/RaftVoteRecord.cpp
+++ src/RaftVoteRecord.cpp
@@ -16,17 +16,16 @@ std::size_t sharpen::RaftVoteRecord::LoadFrom(const char *data,std::size_t size)
     {
         throw sharpen::CorruptedDataError{"corrupted raft vote record"};
     }
-    std::memcpy(&this->term_,data,sizeof(this->term_));
-    std::memcpy(&this->actorId_,data + sizeof(this->term_),sizeof(this->actorId_));
-    return this->ComputeSize();
+    std::size_t offset{0};
+    offset += sharpen::BinarySerializator::LoadFrom(this->term_,data,sizeof(this->term_));
+    offset += sharpen::BinarySerializator::LoadFrom(this->actorId_,data + offset,sizeof(this->actorId_));
+    return offset;
 }
 
 std::size_t sharpen::RaftVoteRecord::UnsafeStoreTo(char *data) const noexcept
 {
     std::size_t offset{0};
-    std::memcpy(data,&this->term_,sizeof(this->term_));
-    offset += sizeof(this->term_);
-    std::memcpy(data + offset,&this->actorId_,sizeof(this->actorId_));
-    offset += sizeof(this->actorId_);
+    offset += sharpen::BinarySerializator::UnsafeStoreTo(this->term_,data);
+    offset += sharpen::BinarySerializator::UnsafeStoreTo(this->actorId_,data + offset);
     return offset;
 }
