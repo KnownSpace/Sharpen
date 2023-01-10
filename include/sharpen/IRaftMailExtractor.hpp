239c30d2e17a8eb322b76171aa78b8339ceeee85
NaturalSharp <2145973003@qq.com>
2023-01-10 21:26:15 +0800

    Refactor interface

--- include/sharpen/IRaftMailExtractor.hpp
+++ include/sharpen/IRaftMailExtractor.hpp
@@ -2,10 +2,13 @@
 #ifndef _SHARPEN_IRAFTMAILEXTRACTOR_HPP
 #define _SHARPEN_IRAFTMAILEXTRACTOR_HPP
 
+#include <stdexcept>
+
 #include "Mail.hpp"
 #include "RaftVoteForRequest.hpp"
 #include "RaftVoteForResponse.hpp"
 #include "RaftMailType.hpp"
+#include "CorruptedDataError.hpp"
 
 namespace sharpen
 {
@@ -18,6 +21,10 @@ namespace sharpen
         virtual bool NviIsRaftMail(const sharpen::Mail &mail) const noexcept = 0;
 
         virtual sharpen::RaftMailType NviGetMailType(const sharpen::Mail &mail) const noexcept = 0;
+
+        virtual sharpen::RaftVoteForRequest NviExtractVoteRequest(const sharpen::Mail &mail) const = 0;
+
+        virtual sharpen::RaftVoteForResponse NviExtractVoteResponse(const sharpen::Mail &mail) const = 0;
     public:
     
         IRaftMailExtractor() noexcept = default;
@@ -39,7 +46,7 @@ namespace sharpen
 
         inline sharpen::RaftMailType GetMailType(const sharpen::Mail &mail) const noexcept
         {
-            if(mail.Empty())
+            if(this->IsRaftMail(mail))
             {
                 return sharpen::RaftMailType::Unknown;
             }
@@ -55,9 +62,23 @@ namespace sharpen
             return this->NviIsRaftMail(mail);
         }
     
-        virtual sharpen::RaftVoteForRequest ExtractVoteRequest(const sharpen::Mail &mail) const = 0;
+        inline sharpen::RaftVoteForRequest ExtractVoteRequest(const sharpen::Mail &mail) const
+        {
+            if(!this->IsRaftMail(mail))
+            {
+                throw std::invalid_argument{"unexpected mail"};
+            }
+            return this->NviExtractVoteRequest(mail);
+        }
 
-        virtual sharpen::RaftVoteForResponse ExtractVoteResponse(const sharpen::Mail &mail) const = 0;
+        inline sharpen::RaftVoteForResponse ExtractVoteResponse(const sharpen::Mail &mail) const
+        {
+            if(!this->IsRaftMail(mail))
+            {
+                throw std::invalid_argument{"unexpected mail"};
+            }
+            return this->NviExtractVoteResponse(mail);
+        }  
     };  
 } 
 
