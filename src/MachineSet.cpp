08c5a36a9a14eb9b8229980a9a82a35a51e2d3be
NaturalSharp <2145973003@qq.com>
2023-02-23 00:00:16 +0800

    Add MachineSet class

---
+++ src/MachineSet.cpp
@@ -0,0 +1,36 @@
+#include <sharpen/MachineSet.hpp>
+
+void sharpen::MachineSet::Insert(std::uint64_t actorId,sharpen::ByteBuffer data)
+{
+    auto ite = this->map_.find(actorId);
+    if(ite != this->map_.end())
+    {
+        ite->second = std::move(data);
+        return;
+    }
+    this->map_.emplace(actorId,std::move(data));
+}
+
+void sharpen::MachineSet::Remove(std::uint64_t actorId) noexcept
+{
+    auto ite = this->map_.find(actorId);
+    if(ite != this->map_.end())
+    {
+        this->map_.erase(ite);
+    }
+}
+
+std::size_t sharpen::MachineSet::GetSize() const noexcept
+{
+    return this->map_.size();
+}
+
+bool sharpen::MachineSet::Empty() const noexcept
+{
+    return this->map_.empty();
+}
+
+void sharpen::MachineSet::Clear() const noexcept
+{
+    this->map_.clear();
+}
