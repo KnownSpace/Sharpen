99694338e809fbb0ba9bd8f60aa58e82ed25fd43
NaturalSharp <2145973003@qq.com>
2023-03-21 23:55:46 +0800

    Add debug tools

---
+++ src/DebugTools.cpp
@@ -0,0 +1,30 @@
+#include <sharpen/DebugTools.hpp>
+
+#include <mutex>
+#include <cstdarg>
+
+#include <sharpen/AsyncMutex.hpp>
+
+sharpen::AsyncMutex *sharpen::InternalSyncPrintMutex() noexcept
+{
+    static sharpen::AsyncMutex mutex;
+    return &mutex;
+}
+
+int sharpen::SyncPrintf(const char *format,...) noexcept
+{
+    sharpen::AsyncMutex *mutex{sharpen::InternalSyncPrintMutex()};
+    std::unique_lock<sharpen::AsyncMutex> lock{*mutex};
+    std::va_list args;
+    va_start(args,format);
+    int result{std::vprintf(format,args)};
+    va_end(args);
+    return result;
+}
+
+int sharpen::SyncPuts(const char *str) noexcept
+{
+    sharpen::AsyncMutex *mutex{sharpen::InternalSyncPrintMutex()};
+    std::unique_lock<sharpen::AsyncMutex> lock{*mutex};
+    return std::puts(str);
+}
