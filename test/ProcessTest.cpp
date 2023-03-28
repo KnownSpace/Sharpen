86d8f1b7666611403c07cce84cd3cf88ff6bc205
NaturalSharp <2145973003@qq.com>
2023-03-29 02:21:31 +0800

    Add Process class

---
+++ test/ProcessTest.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+
+#include <sharpen/Process.hpp>
+#include <sharpen/EventEngine.hpp>
+
+#include <simpletest/TestRunner.hpp>
+
+class RedirectTest :public simpletest::ITypenamedTest<RedirectTest>
+{
+private:
+    using Self = RedirectTest;
+
+public:
+
+    RedirectTest() noexcept = default;
+
+    ~RedirectTest() noexcept = default;
+
+    inline const Self &Const() const noexcept
+    {
+        return *this;
+    }
+
+    inline virtual simpletest::TestResult Run() noexcept
+    {
+#ifdef SHARPEN_IS_NIX
+        std::vector<std::string> args;
+        args.emplace_back("Hello World");
+        sharpen::Process process{"/usr/bin/echo",args.begin(),args.end()};
+        sharpen::InputPipeChannelPtr pOut{process.RedirectStdout()};
+        pOut->Register(sharpen::GetLocalLoopGroup());
+        process.Start();
+        process.Join();
+        sharpen::ByteBuffer buf{4096};
+        std::size_t sz{pOut->ReadAsync(buf)};
+        return this->Assert(sz != 0 && std::strncmp("Hello World",buf.Data(),11) == 0,"Output should equal with \"Hello World\", but it not");
+#else
+        return this->Success();
+#endif
+    }
+};
+
+int Test()
+{
+    simpletest::TestRunner runner;
+    runner.Register<RedirectTest>();
+    return runner.Run();
+}
+
+int main(int argc,char const *argv[])
+{
+    sharpen::EventEngine &engine{sharpen::EventEngine::SetupSingleThreadEngine()};
+    return engine.StartupWithCode(&Test);
+}
