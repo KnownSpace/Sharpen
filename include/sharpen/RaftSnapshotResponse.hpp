96da2c1d7882220c4cd6532d8bcd63a4f56b1d70
NaturalSharp <2145973003@qq.com>
2023-04-07 18:23:51 +0800

    Add RaftSnapshotResponse

---
+++ include/sharpen/RaftSnapshotResponse.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef _SHARPEN_RAFTSNAPSHOTRESPONSE_HPP
+#define _SHARPEN_RAFTSNAPSHOTRESPONSE_HPP
+
+#include <cstdint>
+#include <cstddef>
+#include <utility>
+
+#include "BinarySerializable.hpp"
+
+namespace sharpen
+{
+    class RaftSnapshotResponse:public sharpen::BinarySerializable<sharpen::RaftSnapshotResponse>
+    {
+    private:
+        using Self = sharpen::RaftSnapshotResponse;
+    
+        bool status_;
+        std::uint64_t term_;
+    public:
+    
+        RaftSnapshotResponse() noexcept;
+
+        explicit RaftSnapshotResponse(bool status,std::uint64_t term) noexcept;
+    
+        RaftSnapshotResponse(const Self &other) noexcept = default;
+    
+        RaftSnapshotResponse(Self &&other) noexcept;
+    
+        Self &operator=(const Self &other) noexcept;
+    
+        Self &operator=(Self &&other) noexcept;
+    
+        ~RaftSnapshotResponse() noexcept = default;
+    
+        inline const Self &Const() const noexcept
+        {
+            return *this;
+        }
+
+        inline bool GetStatus() const noexcept
+        {
+            return this->status_;
+        }
+
+        inline void SetStatus(bool status) noexcept
+        {
+            this->status_ = status;
+        }
+
+        inline std::uint64_t GetTerm() const noexcept
+        {
+            return this->term_;
+        }
+
+        inline void SetTerm(std::uint64_t term) noexcept
+        {
+            this->term_ = term;
+        }
+
+        std::size_t ComputeSize() const noexcept;
+
+        std::size_t UnsafeStoreTo(char *data) const noexcept;
+
+        std::size_t LoadFrom(const char *data,std::size_t size);
+    };
+}
+
+#endif
