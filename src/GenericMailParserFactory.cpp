eed2e7549895af510d370a074aa0e91d7957748e
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2023-06-25 08:39:34 +0800

    Revert "[Feature]: raft heartbeat support entires max size limit (#63)" (#64)
    This reverts commit fb88b2c033f35494f7ce67d02b19833d7604d3ac.

--- src/GenericMailParserFactory.cpp
+++ src/GenericMailParserFactory.cpp
@@ -1,41 +1,30 @@
 #include <sharpen/GenericMailParserFactory.hpp>
 
-#include <sharpen/GenericMailParser.hpp>
+#include <sharpen/GenericMailPaser.hpp>
 
 sharpen::GenericMailParserFactory::GenericMailParserFactory(std::uint32_t magic) noexcept
-    : Self{magic, sharpen::GenericMailParser::defaultMaxContentSize} {
-}
-
-sharpen::GenericMailParserFactory::GenericMailParserFactory(std::uint32_t magic,
-                                                            std::uint32_t maxContentSize) noexcept
-    : magic_(magic)
-    , maxContentSize_(maxContentSize) {
+    : magic_(magic) {
 }
 
 sharpen::GenericMailParserFactory::GenericMailParserFactory(Self &&other) noexcept
-    : magic_(other.magic_)
-    , maxContentSize_(other.maxContentSize_) {
+    : magic_(other.magic_) {
     other.magic_ = 0;
-    other.maxContentSize_ = sharpen::GenericMailParser::defaultMaxContentSize;
 }
 
 sharpen::GenericMailParserFactory &sharpen::GenericMailParserFactory::operator=(
     Self &&other) noexcept {
     if (this != std::addressof(other)) {
         this->magic_ = other.magic_;
-        this->maxContentSize_ = other.maxContentSize_;
         other.magic_ = 0;
-        other.maxContentSize_ = sharpen::GenericMailParser::defaultMaxContentSize;
     }
     return *this;
 }
 
 std::unique_ptr<sharpen::IMailParser> sharpen::GenericMailParserFactory::Produce() {
-    std::unique_ptr<sharpen::GenericMailParser> parser{new (std::nothrow)
-                                                     sharpen::GenericMailParser{this->magic_}};
+    std::unique_ptr<sharpen::IMailParser> parser{new (std::nothrow)
+                                                     sharpen::GenericMailPaser{this->magic_}};
     if (!parser) {
         throw std::bad_alloc{};
     }
-    parser->PrepareMaxContentSize(this->maxContentSize_);
     return parser;
 }
